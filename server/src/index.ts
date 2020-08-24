aca91f8110a3923ce8f9024ad8096147655c92dd
Jexxie <1989wjx@gmail.com>
2020-08-24 09:19:18 +0800

    start-change-password

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { buildSchema } from "type-graphql"
 import { HelloResolver } from "./resolvers/hello"
 import { PostResolver } from "./resolvers/post"
 import { UserResolver } from "./resolvers/user"
-import redis from "redis"
+import Redis from "ioredis"
 import session from "express-session"
 import connectRedis from "connect-redis"
 import { __prod__, COOKIE_NAME } from "./constants"
@@ -24,7 +24,7 @@ const main = async () => {
 
 	// make sure the session middleware run before Apollo middleware
 	const RedisStore = connectRedis(session)
-	const redisClient = redis.createClient({
+	const redis = new Redis({
 		host: "127.0.0.1",
 		port: 6379,
 	})
@@ -40,7 +40,7 @@ const main = async () => {
 		session({
 			name: COOKIE_NAME,
 			store: new RedisStore({
-				client: redisClient,
+				client: redis,
 				// 测试环境，用来减少redis请求，生产环境可以设置ttl
 				// disableTTL: true,
 				disableTouch: true,
@@ -62,7 +62,7 @@ const main = async () => {
 			resolvers: [HelloResolver, PostResolver, UserResolver],
 			validate: false,
 		}),
-		context: ({ req, res }) => ({ em: orm.em, req, res }),
+		context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
 	})
 
 	apolloServer.applyMiddleware({
