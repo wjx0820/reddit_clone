dea1a5252d1800e182559d353f50f3f5ec86c6d1
Jexxie <1989wjx@gmail.com>
2020-08-25 11:06:33 +0800

    field-permissions

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -6,6 +6,8 @@ import {
 	Ctx,
 	ObjectType,
 	Query,
+	FieldResolver,
+	Root,
 } from "type-graphql"
 import { MyContext } from "../types"
 import { User } from "../entities/User"
@@ -34,8 +36,18 @@ class UserResponse {
 	user?: User
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+	@FieldResolver(() => String)
+	email(@Root() user: User, @Ctx() { req }: MyContext) {
+		// this is the current user and its ok to show them their own email
+		if (req.session.userId === user.id) {
+			return user.email
+		}
+		// current user wants to see someone elses email
+		return ""
+	}
+
 	@Mutation(() => UserResponse)
 	async changePassword(
 		@Arg("token") token: string,
@@ -158,7 +170,6 @@ export class UserResolver {
 				})
 				.returning("*")
 				.execute()
-			console.log("result>>>>", result)
 			user = result.raw[0]
 		} catch (err) {
 			//|| err.detail.includes("already exists")) {
