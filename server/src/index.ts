803099750434c17b235da2f53f85447a802e6bbe
Jexxie <1989wjx@gmail.com>
2020-08-27 11:54:14 +0800

    more-dataloader

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,7 @@ import { User } from "./entities/User"
 import path from "path"
 import { Updoot } from "./entities/Updoot"
 import { createUserLoader } from "./utils/createUserLoader"
+import { createUpdootLoader } from "./utils/createUpdootLoader"
 
 const main = async () => {
 	// connect to database
@@ -80,6 +81,7 @@ const main = async () => {
 			res,
 			redis,
 			userLoader: createUserLoader(),
+			updootLoader: createUpdootLoader(),
 		}),
 	})
 
