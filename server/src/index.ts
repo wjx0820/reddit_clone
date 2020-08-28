fcc2ccc53e3f2d24bcc2ca1ca48a429f232790bf
Jexxie <1989wjx@gmail.com>
2020-08-28 10:32:30 +0800

    migration

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,7 @@ const main = async () => {
 		type: "postgres",
 		url: process.env.DATABASE_URL,
 		logging: true,
-		synchronize: true,
+		// synchronize: true,
 		migrations: [path.join(__dirname, "./migrations/*")],
 		entities: [Post, User, Updoot],
 	})
@@ -40,6 +40,9 @@ const main = async () => {
 	const RedisStore = connectRedis(session)
 
 	const redis = new Redis(process.env.REDIS_URL)
+
+	// tell express there is NGINX at front
+	app.set("proxy", 1)
 
 	app.use(
 		cors({
